f93dc624335cd11cf5d6b83ac101179bdf38adb6
Arham Sarwar <arhamsarwar786@gmail.com>
2024-02-25 14:48:05 +0500

    refactor code

--- lib/new_components/my_rich_text_screen.dart
+++ lib/new_components/my_rich_text_screen.dart
@@ -1,6 +1,4 @@
-
 import "package:flutter/material.dart";
-
 
 class MyRichTextScreen extends StatelessWidget {
   const MyRichTextScreen({super.key});
@@ -8,24 +6,41 @@ class MyRichTextScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: RichText(text: TextSpan(
-        children: [
-          TextSpan(text: "I", style: TextStyle(color: Colors.red, fontSize: 100, fontWeight: FontWeight.bold )),
-          TextSpan(text: " L", style: TextStyle(color: Colors.blue, fontSize: 100, fontWeight: FontWeight.bold )),
-          TextSpan(text: " o", style: TextStyle(color: Colors.red, fontSize: 100, fontWeight: FontWeight.bold )),
-          TextSpan(text: " v", style: TextStyle(color: Colors.pink, fontSize: 100, fontWeight: FontWeight.bold )),
-          TextSpan(text: " e", style: TextStyle(color: Colors.blue, fontSize: 100, fontWeight: FontWeight.bold )),
-          TextSpan(text: " Pakistan", style: TextStyle(color: Colors.green, fontSize: 100, fontWeight: FontWeight.bold )),
-        ]
-      )),
-     
+      body: RichText(
+          text: TextSpan(children: [
+        TextSpan(
+            text: "I",
+            style: TextStyle(
+                color: Colors.red, fontSize: 100, fontWeight: FontWeight.bold)),
+        TextSpan(
+            text: " L",
+            style: TextStyle(
+                color: Colors.blue,
+                fontSize: 100,
+                fontWeight: FontWeight.bold)),
+        TextSpan(
+            text: " o",
+            style: TextStyle(
+                color: Colors.red, fontSize: 100, fontWeight: FontWeight.bold)),
+        TextSpan(
+            text: " v",
+            style: TextStyle(
+                color: Colors.pink,
+                fontSize: 100,
+                fontWeight: FontWeight.bold)),
+        TextSpan(
+            text: " e",
+            style: TextStyle(
+                color: Colors.blue,
+                fontSize: 100,
+                fontWeight: FontWeight.bold)),
+        TextSpan(
+            text: " Pakistan",
+            style: TextStyle(
+                color: Colors.green,
+                fontSize: 100,
+                fontWeight: FontWeight.bold)),
+      ])),
     );
   }
 }
-
-// Requirments 
-// Bold , 100
-// I Love Pakistan 
-// I = red 
-// Love = Blue 
-// Pakistan = green 
